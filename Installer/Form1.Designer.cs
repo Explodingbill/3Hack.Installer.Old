5e730b9bb9cc67cb24c1e8e6433ab89403d6ca5b
Underkirby <95105680+Underkirby@users.noreply.github.com>
2022-10-16 12:42:32 +1000

    Add files via upload

---
+++ Installer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Installer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.versionText = new System.Windows.Forms.TextBox();
+            this.installInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 463);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial", 7.8F);
+            this.button2.Location = new System.Drawing.Point(156, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(312, 38);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Install";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // versionText
+            // 
+            this.versionText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.versionText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.versionText.Font = new System.Drawing.Font("Arial", 9.8F);
+            this.versionText.ForeColor = System.Drawing.Color.White;
+            this.versionText.Location = new System.Drawing.Point(3, 1);
+            this.versionText.Multiline = true;
+            this.versionText.Name = "versionText";
+            this.versionText.ReadOnly = true;
+            this.versionText.Size = new System.Drawing.Size(610, 86);
+            this.versionText.TabIndex = 7;
+            this.versionText.Text = "3Hack by Explodingbill\r\nThis installer always gets the latest version\r\nso no need" +
+    " to get any other installer";
+            this.versionText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // installInfo
+            // 
+            this.installInfo.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.installInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.installInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.installInfo.Font = new System.Drawing.Font("Arial", 9.8F);
+            this.installInfo.ForeColor = System.Drawing.Color.White;
+            this.installInfo.Location = new System.Drawing.Point(156, 233);
+            this.installInfo.Name = "installInfo";
+            this.installInfo.ReadOnly = true;
+            this.installInfo.Size = new System.Drawing.Size(312, 19);
+            this.installInfo.TabIndex = 8;
+            this.installInfo.Text = "Click to begin installation";
+            this.installInfo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(625, 325);
+            this.Controls.Add(this.installInfo);
+            this.Controls.Add(this.versionText);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.LightPink;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox versionText;
+        private System.Windows.Forms.TextBox installInfo;
+    }
+}
+
---
+++ Installer/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Installer
+{
+    public partial class Form1 : Form
+    {
+        public string dashPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /**/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+
+            client.DownloadDataCompleted += new DownloadDataCompletedEventHandler(client_DownloadDataCompleted);
+
+            string execPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            client.DownloadDataAsync(new Uri("https://github.com/LavaGang/MelonLoader/releases/download/v0.5.4/MelonLoader.x64.zip"), execPath + "\\newfile.zip");
+
+            /*(using (var client = new WebClient())
+            {
+                client.DownloadFile("https://github.com/LavaGang/MelonLoader/releases/download/v0.5.4/MelonLoader.x64.zip", @"C:\Users\talgo\Downloads\E.zi");
+            }*/
+        }
+
+        void client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            MessageBox.Show("File downloaded");
+
+            string execPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            if (!File.Exists(execPath + "\\newfile.zip"))
+            {
+                File.Create(execPath + "\\newfile.zip");
+            }
+
+            File.WriteAllBytes(execPath + "\\newfile.zip", e.Result);//
+        }
+
+
+
+        //DRAGABLE
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "3Dash Executable (*.exe)|*.exe";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                    
+                    DoMelonLoader(filePath, "3Dash.exe");
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void DoMelonLoader(string path, string exeName)
+        {
+            dashPath = path.Replace(exeName, "");
+            installInfo.Text = path;
+
+            string melonPath = path.Replace(exeName, "MelonLoader");
+
+            installInfo.Text = melonPath;
+
+            if (Directory.Exists(melonPath))
+            {
+                installInfo.Text = "Found MelonLoader";
+                InstallMenu();
+            }
+            else
+            {
+                if (File.Exists(dashPath + "Melonx64.zip"))
+                {
+                    ExtractMelonLoader();
+                }
+                else
+                {
+                    installInfo.Text = "Couldn't Find MelonLoader, Installing";
+
+                    using (var client = new WebClient())
+                    {
+                        client.DownloadFile("https://github.com/LavaGang/MelonLoader/releases/download/v0.5.4/MelonLoader.x64.zip", dashPath + "Melonx64.zip");
+
+                        installInfo.Text = "Download Complete, Installing";
+
+                        ExtractMelonLoader();
+                    }
+                }
+            }
+        }
+
+        public void ExtractMelonLoader()
+        {
+            installInfo.Text = "Extracting MelonLoader";
+            ZipFile.ExtractToDirectory(dashPath + "Melonx64.zip", dashPath);
+            installInfo.Text = "Success Installing MelonLoader, Downloading Menu";
+
+            File.Delete(dashPath + "Melonx64.zip");
+
+            InstallMenu();
+        }
+
+        public void InstallMenu()
+        {
+            string url = "https://github.com/Underkirby/3Hack/releases/latest/Hacks.dll";
+
+            using (var client = new WebClient())
+            {
+                if (!Directory.Exists(dashPath + "/Mods/"))
+                {
+                    Directory.CreateDirectory(dashPath + "/Mods/");
+                }
+                else
+                {
+                    if (File.Exists(dashPath + "/Mods/Hacks.dll"))
+                    {
+                        File.Delete(dashPath + "/Mods/Hacks.dll");
+                    }
+                }
+
+                client.DownloadFile(url, dashPath + "/Mods/Hacks.dll");
+
+                installInfo.Text = "Menu Download Part 1 Complete";
+            }
+
+            string url2 = "https://github.com/Underkirby/3Hack/releases/latest/DiscordRPC.dll";
+
+            using (var client2 = new WebClient())
+            {
+                if (!Directory.Exists(dashPath + "/UserLibs/"))
+                {
+                    Directory.CreateDirectory(dashPath + "/UserLibs/");
+                }
+                else
+                {
+                    if (File.Exists(dashPath + "/UserLibs/DiscordRPC.dll"))
+                    {
+                        File.Delete(dashPath + "/UserLibs/DiscordRPC.dll");
+                    }
+                }
+
+                client2.DownloadFile(url2, dashPath + "/UserLibs/DiscordRPC.dll");
+
+                installInfo.Text = "Successfully Downloaded Mod Menu";
+            }
+        }
+    }
+}
